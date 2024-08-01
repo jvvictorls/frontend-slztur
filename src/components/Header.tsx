31034af37c44f2aed82a92d4ec66e1f6de3a418a
João Victor <jvvictorls@hotmail.com>
2024-08-01 20:58:33 -0300

    Feat: Estilização do Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom';
+import '../styles/Header.css';
 
 function Header() {
   return (
-    <header>
-      <h1>SLZtur</h1>
-      <nav>
-        <ul>
+    <header className="header-container">
+      <div className="header-title">
+        <h1 className="titulo">SLZtur</h1>
+      </div>
+      <nav className="menu">
+        <ul className="options">
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/spots">Spots</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
